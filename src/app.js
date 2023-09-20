baff0de62c17dda6090cec765d3ac8c4ab8b51a6
Jessjean17 <140281920+Jessjean17@users.noreply.github.com>
2023-09-20 13:50:43 +0000

    done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,42 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+import "./styles.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const suits = ["hearts", "spades", "clubs", "diamonds"];
+const values = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function getRandomCard() {
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+  return { suit: randomSuit, value: randomValue };
+}
+
+function updateCard() {
+  const card = document.querySelector(".card");
+  const { suit, value } = getRandomCard();
+
+  card.classList.add(suit);
+
+  const numberElement = card.querySelector(".number");
+  numberElement.textContent = value;
+}
+
+window.onload = updateCard();
+
+document.addEventListener("keydown", function(event) {
+  if (event.key === "F5") {
+    updateCard();
+  }
+});
